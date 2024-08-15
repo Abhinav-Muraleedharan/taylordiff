671e91c0c352b70df57ae67f7cd83e5a354ac356
Abhinav Muraleedharan <abhinavmuraleedharan@gmail.com>
2024-08-15 09:57:16 -0400

    added training code

---
+++ main.py
@@ -0,0 +1,18 @@
+import yaml
+import wandb
+from src.data import load_and_preprocess_data
+from src.train import train_model
+
+def main():
+    with open('config/config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+
+    wandb.init(project=config['project']['name'], config=config)
+
+    train_dataset, val_dataset, vocab_size = load_and_preprocess_data(config)
+    trained_state = train_model(config, train_dataset, val_dataset, vocab_size)
+
+    wandb.finish()
+
+if __name__ == "__main__":
+    main()
